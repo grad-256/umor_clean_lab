93b36aa663e0d94596e9b4e3af388adf9bb2206c
grant <miyaudi13937@gmail.com>
2021-11-27 00:15:27 +0900

    更新分

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -5,33 +5,33 @@ export default async function preview(req, res) {
 
   // Check the secret and next parameters
   // This secret should only be known by this API route
-  if (
-    !process.env.WORDPRESS_PREVIEW_SECRET ||
-    secret !== process.env.WORDPRESS_PREVIEW_SECRET ||
-    (!id && !slug)
-  ) {
-    return res.status(401).json({ message: 'Invalid token' })
-  }
+  // if (
+  //   !process.env.WORDPRESS_PREVIEW_SECRET ||
+  //   secret !== process.env.WORDPRESS_PREVIEW_SECRET ||
+  //   (!id && !slug)
+  // ) {
+  //   return res.status(401).json({ message: 'Invalid token' })
+  // }
 
   // Fetch WordPress to check if the provided `id` or `slug` exists
-  const post = await getPreviewPost(id || slug, id ? 'DATABASE_ID' : 'SLUG')
+  // const post = await getPreviewPost(id || slug, id ? 'DATABASE_ID' : 'SLUG')
 
   // If the post doesn't exist prevent preview mode from being enabled
-  if (!post) {
-    return res.status(401).json({ message: 'Post not found' })
-  }
+  // if (!post) {
+  //   return res.status(401).json({ message: 'Post not found' })
+  // }
 
   // Enable Preview Mode by setting the cookies
-  res.setPreviewData({
-    post: {
-      id: post.databaseId,
-      slug: post.slug,
-      status: post.status,
-    },
-  })
+  // res.setPreviewData({
+  //   post: {
+  //     id: post.databaseId,
+  //     slug: post.slug,
+  //     status: post.status,
+  //   },
+  // })
 
   // Redirect to the path from the fetched post
   // We don't redirect to `req.query.slug` as that might lead to open redirect vulnerabilities
-  res.writeHead(307, { Location: `/posts/${post.slug || post.databaseId}` })
-  res.end()
+  // res.writeHead(307, { Location: `/posts/${post.slug || post.databaseId}` })
+  // res.end()
 }
